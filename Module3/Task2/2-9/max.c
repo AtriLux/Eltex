d96b0f9be393de78b0239fdf6ce29b1463fb2923
AtriLux <maria13sivi@yandex.ru>
2023-07-21 00:49:36 +0700

    all channels [except 2-9]

---
+++ Module3/Task2/2-9/max.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(int argc, char* argv[]) {
+    int max = INT_MIN;
+    for (int i = 1; i < argc; i++) {
+        int n = strtol(argv[i], NULL, 10);
+        if (n > max) max = n;
+    }
+    printf("max = %d\n", max);
+}
